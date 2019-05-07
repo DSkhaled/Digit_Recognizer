6902408af0bbc600f85e7001e6402d3d311ac2b6
Khaled YACOUBI <khaled.yacoubi@corp.talan.com>
2019-05-07 16:02:24 +0200

    First Commit

---
+++ Digit_recognizer_CNN.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 24 16:54:49 2019
+
+@author: Inspiron 15
+"""
+#%reset -f
+#%clear
+
+# Importing libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Importing the dataset
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+print('The dimension of the training dataset is: ',train.shape)
+print('The dimension of the testing dataset is: ',test.shape)
+
+# Data preparing
+X_train = train.iloc[:,1:].values
+y_train = train.iloc[:,0].values
+X_test = test.values
+
+# Free some space
+del train
+
+# Normalizing the data
+X_train = X_train/255.0
+X_test = X_test/255.0
+
+# Reshaping the data
+X_train = X_train.reshape(-1,28,28,1)
+X_test = X_test.reshape(-1,28,28,1)
+
+#Label encoding
+from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+y_train =to_categorical(y_train,num_classes=10) 
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size = 0.1, random_state = 0)
+
+
+# Part 1 - Building the CNN
+from keras.models import Sequential
+from keras.layers import Dense,Conv2D,Flatten,MaxPooling2D,Dropout
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import RMSprop
+from keras.callbacks import ReduceLROnPlateau
+
+# Initializing the CNN
+classifier = Sequential()
+
+# First and second layer then Max Pooling adn Dropout
+classifier.add(Conv2D(filters=32,kernel_size=(5,5),padding='same',input_shape=(28,28,1),activation = 'relu'))
+classifier.add(Conv2D(filters=32,kernel_size=(5,5),padding='same',activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+classifier.add(Dropout(0.25))
+
+# Adding the third and fourth convolutional layer then Max Pooling and Dropout
+classifier.add(Conv2D(filters=64,kernel_size=(3,3),padding='same',activation = 'relu'))
+classifier.add(Conv2D(filters=64,kernel_size=(3,3),padding='same',activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+classifier.add(Dropout(0.25))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full Connection neural network
+classifier.add(Dense(units =256 ,activation = 'relu'))
+classifier.add(Dropout(0.5))
+""" In this experience, we just added a fully connected layer trying to improve the CNN
+classifier.add(Dense(units=256,activation='relu'))"""
+classifier.add(Dense(units =10 ,activation = 'softmax'))
+
+""" Setting an optimizer that we are going to use instead of the adam optimizer"""
+# Setting a new optimizer
+optimizer = RMSprop(lr=0.001,rho=0.9,epsilon=1e-08,decay=0.0)
+
+# Compiling the CNN using a new optimizer
+""" We are not gonna use the adam optimizer this time """
+classifier.compile(optimizer = optimizer,loss = 'binary_crossentropy',metrics = ['accuracy'])
+
+# Without data augmentation
+history = classifier.fit(X_train, y_train, batch_size = X_train.shape[0]//30, epochs = 30, validation_data = (X_val, y_val), verbose = 2)
+
+# Using data augmentation to prevent overfitting
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.2, # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+datagen.fit(X_train)
+
+# Set a learning rate annealer
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+
+# Fit the model
+epochs = 30
+batch_size = 100
+history = classifier.fit_generator(datagen.flow(X_train,y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (X_val,y_val),
+                              verbose = 2, steps_per_epoch=X_train.shape[0] // batch_size
+                              , callbacks=[learning_rate_reduction])
+
+# Plot the loss and accuracy curves for training and validation 
+fig, ax = plt.subplots(2,1)
+ax[0].plot(history.history['loss'], color='b', label="Training loss")
+ax[0].plot(history.history['val_loss'], color='r', label="validation loss",axes =ax[0])
+legend = ax[0].legend(loc='best', shadow=True)
+
+ax[1].plot(history.history['acc'], color='b', label="Training accuracy")
+ax[1].plot(history.history['val_acc'], color='r',label="Validation accuracy")
+legend = ax[1].legend(loc='best', shadow=True)
+
+# Look at confusion matrix 
+from sklearn.metrics import confusion_matrix
+import itertools
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Predict the values from the validation dataset
+y_pred = classifier.predict(X_val)
+# Convert predictions classes to one hot vectors 
+Y_pred_classes = np.argmax(y_pred,axis = 1) 
+# Convert validation observations to one hot vectors
+Y_true = np.argmax(y_val,axis = 1) 
+# compute the confusion matrix
+confusion_mtx = confusion_matrix(Y_true, Y_pred_classes) 
+# plot the confusion matrix
+plot_confusion_matrix(confusion_mtx, classes = range(10))
+
+# Display some error results 
+
+# Errors are difference between predicted labels and true labels
+errors = (Y_pred_classes - Y_true != 0)
+
+Y_pred_classes_errors = Y_pred_classes[errors]
+Y_pred_errors = y_pred[errors]
+Y_true_errors = Y_true[errors]
+X_val_errors = X_val[errors]
+
+def display_errors(errors_index,img_errors,pred_errors, obs_errors):
+    """ This function shows 6 images with their predicted and real labels"""
+    n = 0
+    nrows = 2
+    ncols = 3
+    fig, ax = plt.subplots(nrows,ncols,sharex=True,sharey=True)
+    for row in range(nrows):
+        for col in range(ncols):
+            error = errors_index[n]
+            ax[row,col].imshow((img_errors[error]).reshape((28,28)))
+            ax[row,col].set_title("Predicted label :{}\nTrue label :{}".format(pred_errors[error],obs_errors[error]))
+            n += 1
+
+# Probabilities of the wrong predicted numbers
+Y_pred_errors_prob = np.max(Y_pred_errors,axis = 1)
+
+# Predicted probabilities of the true values in the error set
+true_prob_errors = np.diagonal(np.take(Y_pred_errors, Y_true_errors, axis=1))
+
+# Difference between the probability of the predicted label and the true label
+delta_pred_true_errors = Y_pred_errors_prob - true_prob_errors
+
+# Sorted list of the delta prob errors
+sorted_dela_errors = np.argsort(delta_pred_true_errors)
+
+# Top 6 errors 
+most_important_errors = sorted_dela_errors[-6:]
+
+# Show the top 6 errors
+display_errors(most_important_errors, X_val_errors, Y_pred_classes_errors, Y_true_errors)
+
+# predict results
+results = classifier.predict(X_test)
+
+# select the index with the maximum probability
+results = np.argmax(results,axis = 1)
+
+results = pd.Series(results,name="Label")
+
+
+# Submission
+submission = pd.concat([pd.Series(range(1,28001),name = "ImageId"),results],axis = 1)
+
+submission.to_csv("digit_recognizer_CNN.csv",index=False)
+
